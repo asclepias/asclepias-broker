f53973541b44d143a0a79b5283afe1b3e644979d
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-10-13 16:24:17 +0100

    Added initial data model

---
+++ asclepias_broker/broker.py
@@ -0,0 +1,57 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from .datastore import Organization, Identifier, Type, Object, Base
+
+
+def get_or_create(session, model, **kwargs):
+    # https://stackoverflow.com/a/2587041/180783
+    instance = session.query(model).filter_by(**kwargs).first()
+    if instance:
+        return instance
+    else:
+        instance = model(**kwargs)
+        session.add(instance)
+        instance = session.query(model).filter_by(**kwargs).first()
+        return instance
+
+
+class SoftwareBroker(object):
+
+    def __init__(self):
+        self.engine = create_engine('sqlite:///:memory:', echo=False)
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
+        Base.metadata.create_all(self.engine)
+
+    def handle_event(self, event):
+        handler = getattr(self, event['event_type'])
+        handler(event)
+
+    def object_created(self, event):
+
+        for payload in event['payload']:
+
+            obj_payload = payload['object']
+
+            publisher = get_or_create(self.session, Organization, **obj_payload['publisher'])
+            identifier = get_or_create(self.session, Identifier, **obj_payload['identifier'])
+            object_type = get_or_create(self.session, Type, **obj_payload['type'])
+            publication_date = obj_payload['publication_date']
+
+            obj = Object(publisher_id=publisher.id, identifier_id=identifier.id,
+                         type_id=object_type.id, publication_date=publication_date)
+
+            self.session.add(publisher)
+            self.session.add(identifier)
+            self.session.add(object_type)
+            self.session.add(obj)
+            self.session.commit()
+
+    def relation_created(self, event):
+        pass
+
+    def show_all(self):
+        for cls in [Organization, Identifier, Type, Object]:
+            for obj in self.session.query(cls):
+                print(obj)
---
+++ asclepias_broker/datastore.py
@@ -0,0 +1,130 @@
+# For now this is a toy datastore that is completely unoptimized
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, ForeignKey, Integer, Enum
+# from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class Identifier(Base):
+    """
+    A persistent Identifier
+    """
+    __tablename__ = 'identifiers'
+    id = Column(String, primary_key=True)
+    id_schema = Column(String)
+    id_url = Column(String)
+
+    def __repr__(self):
+        return (f'Identifier: '
+                f'id={self.id} '
+                f'id_schema={self.id_schema} '
+                f'id_url={self.id_url}')
+
+
+class Type(Base):
+    """
+    An object type
+    """
+    __tablename__ = 'types'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    sub_type = Column(String)
+    sub_type_schema = Column(String)
+
+    def __repr__(self):
+        return (f'Type: '
+                f'id={self.id} '
+                f'name={self.name} '
+                f'sub_type={self.sub_type} '
+                f'sub_type_schema={self.sub_type_schema}')
+
+
+class RelationshipType(Base):
+    """
+    A relationship type
+    """
+    __tablename__ = 'relationship_types'
+    id = Column(Integer, primary_key=True)
+    scholix_relationship = Enum("IsReferencedBy", "References", "IsSupplementTo",
+                                "IsSupplementedBy", "IsRelatedTo")
+    original_relationship_name = Enum("IsCitedBy", "Cites", "IsSupplementTo",
+                                      "IsSupplementedBy", "IsContinuedBy",
+                                      "Continues", "HasMetadata",
+                                      "IsMetadataFor" "IsNewVersionOf",
+                                      "IsPreviousVersionOf", "IsPartOf",
+                                      "HasPart", "IsReferencedBy", "References",
+                                      "IsDocumentedBy", "Documents",
+                                      "IsCompiledBy", "Compiles",
+                                      "IsVariantFormOf", "IsOriginalFormOf",
+                                      "IsIdenticalTo", "IsReviewedBy",
+                                      "Reviews", "IsDerivedFrom", "IsSourceOf",
+                                      "IsDescribedBy", "Describes",
+                                      "HasVersion", "IsVersionOf",
+                                      "IsRequiredBy", "Requires")
+    original_relationship_schema = Enum("DataCite")
+
+    def __repr__(self):
+        return (f'Organization: '
+                f'scholix_relationship={self.scholix_relationship} '
+                f'original_relationship_name={self.original_relationship_name} '
+                f'original_relationship_schema={self.original_relationship_schema}')
+
+
+class Organization(Base):
+    __tablename__ = 'organizations'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    identifier = Column(String, ForeignKey('identifiers.id'))
+
+    def __repr__(self):
+        return (f'Organization: '
+                f'id={self.id} '
+                f'name={self.name} '
+                f'identifier={self.identifier}')
+
+
+class Object(Base):
+
+    __tablename__ = 'objects'
+
+    id = Column(Integer, primary_key=True)
+
+    identifier_id = Column(String, ForeignKey('identifiers.id'))
+    type_id = Column(Integer, ForeignKey('types.id'))
+    publisher_id = Column(String, ForeignKey('organizations.id'))
+    publication_date = Column(String)
+
+    def __repr__(self):
+        return (f'Object: '
+                f'identifier_id={self.identifier_id} '
+                f'type_id={self.type_id} '
+                f'publisher_id={self.publisher_id} '
+                f'publication_date={self.publication_date}')
+
+
+class Relationship(Base):
+
+    __tablename__ = 'relationships'
+
+    id = Column(Integer, primary_key=True)
+    source_id = Column(String, ForeignKey('objects.id'))
+    target_id = Column(String, ForeignKey('objects.id'))
+    relationship_type = Column(String, ForeignKey('relationship_types.id'))
+
+    def __repr__(self):
+        return (f'Relationship: '
+                f'source={self.source_id} '
+                f'relationship_type={self.relationship_type} '
+                f'target={self.target_id}')
+
+    # object1 = relationship("Entity", foreign_keys=[object1_id], back_populates="relationships")
+    # object2 = relationship("Entity", foreign_keys=[object2_id], back_populates="relationships")
+
+    # def __repr__(self):
+    #     return ("<Relationship(object_1='%s', relationship_type='%s', object_2='%s')>" %
+    #             (self.object_1_id, self.relationship_type, self.object_2_id))
+
+# Entity.relationships = relationship("Relationship", order_by=Relationship.id,
+#                                     primaryjoin=or_(Entity.id == Relationship.object_1_id, Entity.id == Relationship.object_2_id))
