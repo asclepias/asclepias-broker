09b533697b36b14cb619d4ca18ebaf0200435114
Alexander Ioannidis <a.ioannidis@cern.ch>
2018-01-23 17:17:53 +0100

    setup: add missing dependencies

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@ from setuptools import setup
 
 from asclepias_broker import __version__
 
+tests_require = ['pytest', 'jsonschema']
+install_requires = ['sqlalchemy', 'sqlalchemy_utils']
+extras_require = {
+    'tests': tests_require,
+}
+
 setup(version=__version__,
       url="https://github.com/astrofrog/asclepias-toy-broker",
       name="asclepias-broker",
       description='Prototype broker code for the Asclepias project',
       packages=['asclepias_broker'],
-      install_requires=['sqlalchemy'],
+      extras_require=extras_require,
+      tests_require=tests_require,
+      install_requires=install_requires,
       license='BSD',
       author='Thomas Robitaille',
       author_email='thomas.robitaille@gmail.com')
