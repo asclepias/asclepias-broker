d1531791e80eda358daff3d52978f32742d2f3b3
Alexander Ioannidis <a.ioannidis@cern.ch>
2018-01-24 17:40:13 +0100

    tests: test simple citations

--- tests/query/test_citations.py
+++ tests/query/test_citations.py
@@ -2,79 +2,123 @@
 import pytest
 
 from asclepias_broker.datastore import Identifier
+from collections import OrderedDict
 
 
-def common_items(a, b):
-    return [(a[k], b[k]) for k in a.keys() & b.keys()]
+TEST_CASES = OrderedDict({
+    'no_citations': (
+        [
+            ['C', 'A', 'IsSupplementTo', 'B', '2018-01-01'],
+        ],
+        {
+            'A': [set(), set()],
+            'B': [set(), set()],
+        }
+    ),
+    'one_identity': (
+        [
+            ['C', 'A', 'IsIdenticalTo', 'B', '2018-01-01'],
+        ],
+        {
+            'A': [set(), set()],
+            'B': [set(), set()],
+        }
+    ),
+    'two_identities': (
+        [
+            ['C', 'A', 'IsIdenticalTo', 'B', '2018-01-01'],
+            ['C', 'B', 'IsIdenticalTo', 'C', '2018-01-01'],
+        ],
+        {
+            'A': [set(), set()],
+            'B': [set(), set()],
+            'C': [set(), set()],
+        }
+    ),
+    'one_citation': (
+        [
+            ['C', 'A', 'Cites', 'B', '2018-01-01'],
+        ],
+        {
+            'A': [set(), set()],
+            'B': [{'A'}, {('A', 'B')}],
+        }
+    ),
+    'two_citations': (
+        [
+            ['C', 'A', 'Cites', 'B', '2018-01-01'],
+            ['C', 'C', 'Cites', 'B', '2018-01-01'],
+        ],
+        {
+            'A': [set(), set()],
+            'B': [{'A', 'C'}, {('A', 'B'), ('C', 'B')}],
+            'C': [set(), set()],
+        }
+    ),
+    'one_indirect_citation': (
+        [
+            ['C', 'A', 'IsIdenticalTo', 'B', '2018-01-01'],
+            ['C', 'C', 'Cites', 'B', '2018-01-01'],
+        ],
+        {
+            'A': [{'C'}, {('C', 'B')}],
+            'B': [{'C'}, {('C', 'B')}],
+            'C': [set(), set()],
+        }
+    ),
+    'two_indirect_citations': (
+        [
+            ['C', 'A', 'IsIdenticalTo', 'B', '2018-01-01'],
+            ['C', 'C', 'IsIdenticalTo', 'A', '2018-01-01'],
+            ['C', 'X', 'Cites', 'B', '2018-01-01'],
+            ['C', 'Y', 'Cites', 'C', '2018-01-01'],
+        ],
+        {
+            'A': [{'X', 'Y'}, {('X', 'B'), ('Y', 'C')}],
+            'B': [{'X', 'Y'}, {('X', 'B'), ('Y', 'C')}],
+            'C': [{'X', 'Y'}, {('X', 'B'), ('Y', 'C')}],
+            'X': [set(), set()],
+            'Y': [set(), set()],
+        }
+    ),
+    'two_indirect_identical_citations': (
+        [
+            ['C', 'A', 'IsIdenticalTo', 'B', '2018-01-01'],
+            ['C', 'C', 'IsIdenticalTo', 'A', '2018-01-01'],
+            ['C', 'X', 'Cites', 'B', '2018-01-01'],
+            ['C', 'Y', 'Cites', 'C', '2018-01-01'],
+            ['C', 'Y', 'IsIdenticalTo', 'X', '2018-01-01'],
+        ],
+        # NOTE: Since the `expand_target` is not called, this still counts two
+        # unique citations.
+        {
+            'A': [{'X', 'Y'}, {('X', 'B'), ('Y', 'C')}],
+            'B': [{'X', 'Y'}, {('X', 'B'), ('Y', 'C')}],
+            'C': [{'X', 'Y'}, {('X', 'B'), ('Y', 'C')}],
+            'X': [set(), set()],
+            'Y': [set(), set()],
+        }
+    ),
+})
 
 
-TEST_STATES = {
-    'no_citations': [
-        ['C', 'A', 'IsSupplementTo', 'B', '2018-01-01'],
-    ],
-    'one_citation': [
-        ['C', 'A', 'Cites', 'B', '2018-01-01'],
-    ],
-    'one_identity': [
-        ['C', 'A', 'IsIdenticalTo', 'B', '2018-01-01'],
-    ],
-    'two_identities': [
-        ['C', 'A', 'IsIdenticalTo', 'B', '2018-01-01'],
-        ['C', 'B', 'IsIdenticalTo', 'C', '2018-01-01'],
-    ],
-    'three_identities': [
-        ['C', 'A', 'IsIdenticalTo', 'B', '2018-01-01'],
-        ['C', 'B', 'IsIdenticalTo', 'C', '2018-01-01'],
-        ['C', 'C', 'IsIdenticalTo', 'A', '2018-01-01'],
-    ],
-    'two_identities_and_other_relationships': [
-        ['C', 'A', 'IsIdenticalTo', 'B', '2018-01-01'],
-        ['C', 'B', 'IsIdenticalTo', 'C', '2018-01-01'],
-        ['C', 'X', 'Cites', 'B', '2018-01-01'],
-        ['C', 'A', 'Cites', 'Y', '2018-01-01'],
-    ],
-    'two_identities_and_other_relationships_2': [
-        ['C', 'A', 'IsIdenticalTo', 'B', '2018-01-01'],
-        ['C', 'B', 'IsIdenticalTo', 'C', '2018-01-01'],
-        ['C', 'X', 'Cites', 'Y', '2018-01-01'],
-    ],
-    'two_groups_of_identities': [
-        ['C', 'A', 'IsIdenticalTo', 'B', '2018-01-01'],
-        ['C', 'B', 'IsIdenticalTo', 'C', '2018-01-01'],
-        ['C', 'X', 'IsIdenticalTo', 'Y', '2018-01-01'],
-    ],
-    'two_groups_of_identities2': [
-        ['C', 'A', 'IsIdenticalTo', 'B', '2018-01-01'],
-        ['C', 'B', 'IsIdenticalTo', 'C', '2018-01-01'],
-        ['C', 'X', 'IsIdenticalTo', 'Y', '2018-01-01'],
-        ['C', 'X', 'IsIdenticalTo', 'Z', '2018-01-01'],
-    ],
-    'five_identities': [
-        ['C', 'A', 'IsIdenticalTo', 'B', '2018-01-01'],
-        ['C', 'B', 'IsIdenticalTo', 'C', '2018-01-01'],
-        ['C', 'X', 'IsIdenticalTo', 'Y', '2018-01-01'],
-        ['C', 'X', 'IsIdenticalTo', 'Z', '2018-01-01'],
-        ['C', 'Z', 'IsIdenticalTo', 'A', '2018-01-01'],
-    ],
-}
-
-
-NO_ARGS_TEST_CASES = {
-    'no_citations': {'A': set(), 'B': set()},
-    'one_citation': {{'B': {'A'}}},
-    # 'one_citation': {{'B': {'A'}}},
-    # 'one_citation': {{'B': {'A'}}},
-    # 'one_citation': {{'B': {'A'}}},
-    # 'one_citation': {{'B': {'A'}}},
-    # 'one_citation': {{'B': {'A'}}},
-}
+def spread_test_cases_dict(d):
+    return [(name, case_data[0], case_data[1])
+            for name, case_data in d.items()]
 
 
 @pytest.mark.parametrize(
-    ('broker', 'results'), common_items(TEST_STATES, NO_ARGS_TEST_CASES),
+    ('test_case', 'broker', 'results'),
+    spread_test_cases_dict(TEST_CASES),
     indirect=['broker'])
-def test_no_args_citations(broker, result_sets):
-    for id_value, result in result_sets.items():
-        id_ = broker.session.query(Identifier).filter_by(value=id_value).one()
-        citations = set(i.value for i in broker.get_citations(id_))
-        assert citations == result
+def test_simple_citations(test_case, broker, results):
+    for cited_id_value, (citation_result, relation_result) in results.items():
+        cited_id = (broker.session.query(Identifier)
+                    .filter_by(value=cited_id_value).one())
+        citations = set()
+        relations = set()
+        for citing_id, relation in broker.get_citations(cited_id):
+            citations |= {i.value for i in citing_id}
+            relations |= {(r.source.value, r.target.value) for r in relation}
+        assert citations == citation_result
+        assert relations == relation_result
